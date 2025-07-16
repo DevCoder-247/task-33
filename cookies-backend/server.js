c96ae7dc81f7b8d04561eca5e6b19f24bef8a681
DevCoder-247 <chikteabhay@gmail.com>
2025-07-16 17:44:45 +0530

    updated port

--- cookies-backend/server.js
+++ cookies-backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app  =  express();
-const port = 3000 || process.env.PORT;
+const port =  process.env.PORT;
 
 //middleware setup
 app.use(cors({
